68d64fd9aaf1dcc08a2e0261ea63fc38d1d8b402
Alexander Kalashnikov <akalashnikov@optimalcity.ru>
2023-12-12 12:45:44 +0300

    tokenize

--- cwords.c
+++ cwords.c
@@ -1,5 +1,8 @@
-#include <stdio.h>
+#include <sys/types.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 static int process_file(FILE *);
 
@@ -21,7 +24,7 @@ int main(int argc, const char *argv[])
         printf("ERROR: could not open your file %s.\n", argv[1]);
         return 2;
     }
-    if (process_file(input) != 0)
+    if (process_file(input) < 0)
         printf("FAIL.\n");
     else
         printf("OK.\n");
@@ -30,17 +33,74 @@ int main(int argc, const char *argv[])
     return 0;
 }
 
-int process_file(FILE *input)
+static const char *separators = "\n \t,.;-*&?!\'\"()[]{}";
+
+int process_line(char *line)
 {
-    while (!feof(input) && !ferror(input))
+    printf("%s", line);
+    int len = strlen(line);
+    int current = 0;
+    while (current < len)
     {
-        char *line = NULL;
-        size_t len = 0;
-        getline(&line, &len, input);
-        printf("%s", line);
-        if (line)
-            free(line);
+        /* skip separators */
+        while (current < len)
+        {
+            if (strchr(separators, line[current]) == NULL)
+                break;
+            ++current;
+        }
+        int start = current;
+        /* take word */
+        while (current < len)
+        {
+            if (strchr(separators, line[current]) != NULL)
+                break;
+            ++current;
+        }
+        int size = current - start;
+        if (size > 0)
+        {
+            char *word = malloc(size + 1);
+            for (int i = 0; i < size; ++i)
+            {
+                word[i] = line[start + i];
+            }
+            word[size] = '\0';
+            printf("%s|", word);
+            free(word);
+        }
     }
+    printf("\n");
 
     return 0;
+}
+
+int process_file(FILE *input)
+{
+    size_t count = 0;
+    char *line = NULL;
+    int result = 0;
+    while (!feof(input) && !ferror(input))
+    {
+        line = NULL;
+        size_t size = 0;
+        ssize_t len = getline(&line, &size, input);
+        if (len < 0)
+        {
+            printf("ERROR: could not read line.\n");
+            result = -1;
+            break;
+        }
+        else
+        {
+            printf("%lu. (%ld - %lu)", ++count, len, size);
+            result = process_line(line);
+        }
+        if (line)
+            free(line);
+        if (result < 0)
+            break;
+    }
+    printf("\n");
+    return result;
 }
